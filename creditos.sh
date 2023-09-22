e3eb8d5c3bb01eb5e6962623c04cdb4b552d5c7f
Mitzy <mitzy.yanet10@gmail.com>
2023-09-22 17:58:13 -0600

    Los respectivos creditos de los programadores

---
+++ creditos.sh
@@ -0,0 +1,5 @@
+echo "PPPPP   EEEEE   PPPP   EEEEE    Y   Y    M     M  III  TTTTTT  ZZZZZ    Y   Y"
+echo "P    P  E       P   P  E        Y   Y    MM   MM   I      T       Z     Y   Y"
+echo "PPPPP   EEEEE   PPPP   EEEEE     Y Y     M M M M   I      T      Z       Y Y"
+echo "P       E       P      E          Y      M  M  M   I      T     Z         Y"
+echo "P       EEEEE   P      EEEEE      Y      M     M  III     T    ZZZZZ      Y"
