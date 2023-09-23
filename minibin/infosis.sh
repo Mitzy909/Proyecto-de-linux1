c116f0c9715a5ccfcb2ef95cc90cab9311261e96
Mitzy <mitzy.yanet10@gmail.com>
2023-09-22 20:03:14 -0600

    Agregue dibujitos

--- minibin/infosis.sh
+++ minibin/infosis.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-
-# Información de Memoria RAM desde /proc/meminfo
-memoria_ram=$(grep 'MemTotal' /proc/meminfo | awk '{print $2}')
-
+  GNU nano 6.2                                                          infosis.sh                                                                    
 # Arquitectura del Sistema desde /proc/version
 arquitectura=$(grep 'model name' /proc/cpuinfo | head -n 1 | awk '{print $4}')
 if [[ "$arquitectura" == "lm" ]]; then
@@ -13,8 +9,22 @@ fi
 # Versión del SO desde /etc/os-release
 version_so=$(grep 'PRETTY_NAME' /etc/os-release | awk -F '=' '{print $2}' | tr -d '"')
 
+
+echo "-----------------------------------"
+echo "|         I N F O S I S            |"
+echo "-----------------------------------"
+
 echo "Información del sistema:"
 echo "Memoria RAM: $memoria_ram kB"
 echo "Arquitectura del sistema: $arquitectura"
 echo "Versión del SO: $version_so"
 
+
+echo ""
+echo "---------------------"
+echo "███▀▀▀▀███████████████"
+echo "██░░░░░▄██████████████"
+echo "█▌░░░░████░░██░░██░░██"
+echo "██░░░░░▀██████████████"
+echo "███▄▄▄▄███████████████"
+echo "---------------------"
