fea3709b4d53a0bcecb5264e2cd3f1e134ffc784
JoseP <joseperalta045@gmail.com>
2023-09-22 21:04:03 -0600

    aqui va la super terminal

---
+++ terminal2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+trap "" SIGINT SIGTSTP
+
+current_directory=$(pwd)
+minibin_directory="${current_directory}/minibin"
+export PATH=$PATH:$minibin_directory
+
+
+
+
+usuario=$USER
+directorio_actual=${PWD##*/}
+
+while true; do
+    read -p "$usuario@$directorio_actual@MiTerminal:~$ " comando
+
+    # Verificar si se ingresó "salir" para salir del script
+    if [[ $comando == "salir" ]]; then
+        echo "Saliendo de la Terminal."
+        exit 0
+    fi
+
+    # Ejecutar el comando ingresado
+    eval "$comando"
+done
